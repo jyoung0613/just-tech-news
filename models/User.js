c668392eccc7ecf14d4410ef335a1e267daf442b
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-10-12 17:40:24 -0600

    password bcryptjsed

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcryptjs = require('bcryptjs');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcryptjs.compareSync(loginPw, this.password);
+    }
+}
 
 // define table columns and configuration
 User.init(
@@ -45,6 +51,20 @@ User.init(
         }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData){
+                newUserData.password = await bcryptjs.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcryptjs.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+
+        },
+
         sequelize,
         timestamps: false,
         freezeTableName: true,
