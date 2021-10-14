42a36ef4494b444e22d3c1c72e5daa629a65ce92
John Young <87405768+jyoung0613@users.noreply.github.com>
2021-10-14 10:48:33 -0600

    added Vote js file and included the necessary connections to tables through index js in models

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Post = require('./Post');
 const User = require('./User');
+const Vote = require('./Vote');
 
 // create associations
 User.hasMany(Post, {
@@ -10,4 +11,33 @@ Post.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Post };
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+
+module.exports = { User, Post, Vote };
